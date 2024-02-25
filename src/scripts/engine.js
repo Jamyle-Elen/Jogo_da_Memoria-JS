7c2c965b1ceedf2651f1d1de210fae0989d63a58
Jamyle <110051309+Jamyle-Elen@users.noreply.github.com>
2024-02-25 18:16:21 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -24,6 +24,7 @@ let shuffleIcons = icons.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
 for (let i = 0; i < icons.length; i++) {
     let box = document.createElement("div");
+    box.className = "item";
     box.innerHTML = shuffleIcons[i];
     document.querySelector(".game").appendChild(box);
     
